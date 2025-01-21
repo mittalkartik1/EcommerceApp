d46d9f047b0b31b3a5cc1f3bf8e80ef13990d14e
kartikmittal1996 <113685476+kartikmittal1996@users.noreply.github.com>
2025-01-22 01:04:24 +0530

    Added viewpager autoscroll

--- src/screens/homeScreen/components/FeaturedProducts.tsx
+++ src/screens/homeScreen/components/FeaturedProducts.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useRef} from 'react';
 import {
     ActivityIndicator,
     Animated,
@@ -18,6 +18,7 @@ import {useDispatch, useSelector} from 'react-redux';
 import {fetchFeaturedProducts} from '../../../redux/actions/productsAction';
 import {RootState} from '../../../redux/store';
 import PagerView from 'react-native-pager-view';
+import { useFocusEffect } from '@react-navigation/native';
 
 const FeaturedProducts = () => {
     const {width} = Dimensions.get('window');
@@ -29,11 +30,22 @@ const FeaturedProducts = () => {
         (state: RootState) => state.products.featuredProducts,
     );
     const pageMargin = 0 - (Number((0.25 * width).toFixed(2)) - 15);
+    const viewPagerRef = useRef<PagerView>(null);
+    const pageRef = useRef(0);
 
     useEffect(() => {
         dispatch(fetchFeaturedProducts());
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
+
+    useFocusEffect(
+        React.useCallback(() => {
+            const intervalId = setInterval(() => viewPagerRef.current?.setPage((pageRef.current + 1) % 4), 2000);
+            return(() => {
+                clearInterval(intervalId);
+            });
+        }, [])
+    );
 
     const bannerItem = ({item, index}: {item: BannerItem; index: number}) => (
         <View
@@ -60,8 +72,12 @@ const FeaturedProducts = () => {
             <Text style={styles.headerText}>Featured</Text>
             <AnimatedPagerView
                 testID="pager-view"
+                ref={viewPagerRef}
                 style={styles.viewpager}
                 offscreenPageLimit={4}
+                onPageSelected={e => {
+                    pageRef.current = e.nativeEvent.position;
+                }}
                 pageMargin={isAndroid ? Math.floor(pageMargin) : 10}>
                 {featuredProductsState.data.map((item ,index) => bannerItem({item, index}))}
             </AnimatedPagerView>
